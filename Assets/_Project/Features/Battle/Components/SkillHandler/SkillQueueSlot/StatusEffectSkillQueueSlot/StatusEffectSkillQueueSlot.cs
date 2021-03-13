cde2182bca8159ee19e96716875ad49f279e1e43
Richard Shaw <richardmorganshaw@gmail.com>
2021-03-13 14:59:48 -0800

    Changed StatusEffectSkillQueueSlot def

--- Assets/_Project/Features/Battle/Components/SkillHandler/SkillQueueSlot/StatusEffectSkillQueueSlot/StatusEffectSkillQueueSlot.cs
+++ Assets/_Project/Features/Battle/Components/SkillHandler/SkillQueueSlot/StatusEffectSkillQueueSlot/StatusEffectSkillQueueSlot.cs
@@ -5,12 +5,12 @@ public class StatusEffectSkillQueueSlot : SkillQueueSlot
     List<float> values;
 
     public StatusEffectSkillQueueSlot(
-        Skill _skill,
         CharacterSlot _caster,
         CharacterSlot _target,
+        Skill _skill,
         List<float> _values
     ) :
-        base(_skill, _caster, _target)
+        base(_caster, _target, _skill)
     {
         values = _values;
     }
